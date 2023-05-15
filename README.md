6ce9db9b4330146fdc3b9e87f9e854c368740c39
nnguyet <nguyetlms@gmail.com>
2023-05-15 22:38:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Install libraries
+
+```cmd
+pip install -r requirements.txt
+```
+
+## Run local
+
+```cmd
+flask run
+```
+
+## API
+>   /predict/?FFMC=***val***&DMC=***val***&DC=***val***&ISI=***val***&temp=***val***&RH=***val***&wind=***val***&rain=***val***
+
+* Data test
+>   /predict/?FFMC=80.2&DMC=11.7&DC=90.4&ISI=2.8&temp=36&RH=61&wind=18&rain=0.262364
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,request
+from flask_restful import Resource, Api
+from tensorflow import keras
+import numpy as np
+from flask_cors import CORS
+
+COLUMNS = ['FFMC','DMC','DC','ISI','temp','RH','wind','rain']
+
+app = Flask(__name__)
+#
+CORS(app)
+# creating an API object
+api = Api(app)
+
+#prediction api call
+class predict(Resource):
+    def get(self):
+        # Get data
+        FFMC = request.args.get('FFMC')
+        DMC = request.args.get('DMC')
+        DC = request.args.get('DC')
+        ISI = request.args.get('ISI')
+        temp = request.args.get('temp')
+        RH = request.args.get('RH')
+        wind = request.args.get('wind')
+        rain = request.args.get('rain')
+
+        data = np.array([[float(FFMC),float(DMC),float(DC),float(ISI),float(temp),float(RH),float(wind),float(rain)]])
+        
+        # Load model
+        model = keras.models.load_model('model.h5', compile=False)
+
+        # Predict
+        prediction = model.predict(data)
+        prediction = float(prediction[0])
+
+        return prediction
+
+#
+api.add_resource(predict, '/predict/')
+
+if __name__ == '__main__':
+    app.run()
